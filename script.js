821e84f0720f2408a6ab368d4b418d89e2f5b806
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-02 15:51:26 -0500

    completed

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const spacing = document.querySelector("input[type= 'range']");
+// const spacing = document.querySelector("input[type= 'range']");
 
-const blur = document.getElementById('#blur');
+// const blur = document.getElementById('#blur');
+
+const inputs = document.querySelectorAll('input')
+
+
+function handleUpdate(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+inputs.forEach(inputs => inputs.addEventListener('change', handleUpdate)) //for each inputs run the addEventListener function which is attached to the inputs element
+
+inputs.forEach(inputs => inputs.addEventListener('mousemove', handleUpdate))
